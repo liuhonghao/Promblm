459af791e51cbe48c4d6cded68d601b22ec18baa
刘弘浩 <76436932+liuhonghao@users.noreply.github.com>
2021-05-04 22:12:47 +0800

    Add files via upload

---
+++ B.Specialized Four-Digit Numbers/B.Specialized Four-Digit Numbers/T.cpp
@@ -0,0 +1,27 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int mod(int n, int b)
+{
+	int sum = 0;
+	while (n)
+	{
+		sum += n%b;
+		n /= b;
+	}
+	return sum;
+}
+int main()
+{
+	for (int i = 2292; i < 10000; i++)
+	{
+		if (mod(i, 10) == mod(i, 12) && mod(i, 10) == mod(i, 16))
+		{
+			printf("%d\n", i);
+		}
+	}
+	return 0;
+}
