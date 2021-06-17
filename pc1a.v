2b0eef666ed04da89ab19d2d35f627a46f923063
刘弘浩 <76436932+liuhonghao@users.noreply.github.com>
2021-06-17 22:32:08 +0800

    Add files via upload

---
+++ pc1a.v
@@ -0,0 +1,28 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2021/05/30 19:08:32
+// Design Name: 
+// Module Name: pc1a
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module pc1a(
+    input [7:0] x,
+    output [2:0] y,
+    output valid
+    );
+endmodule
